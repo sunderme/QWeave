ece73beda6a8ac8c746f2033087a5caaf64b0453
Jan Sundermeyer <sunderme@gmx.de>
2022-09-14 20:32:56 +0200

    update translations

--- translation/qweave_de.ts
+++ translation/qweave_de.ts
@@ -1,4 +1,309 @@
 <?xml version="1.0" encoding="utf-8"?>
 <!DOCTYPE TS>
 <TS version="2.1" language="de_DE">
+<context>
+    <name>DuplicatPatternDlg</name>
+    <message>
+        <location filename="../src/duplicatpatterndlg.cpp" line="5"/>
+        <source>Duplicate Pattern</source>
+        <translation>Dupliziere Muster</translation>
+    </message>
+    <message>
+        <location filename="../src/duplicatpatterndlg.cpp" line="6"/>
+        <source>shift by:</source>
+        <translation>verschiebe um:</translation>
+    </message>
+    <message>
+        <location filename="../src/duplicatpatterndlg.cpp" line="7"/>
+        <source>times:</source>
+        <translation>mal:</translation>
+    </message>
+    <message>
+        <location filename="../src/duplicatpatterndlg.cpp" line="19"/>
+        <source>OK</source>
+        <translation>OK</translation>
+    </message>
+    <message>
+        <location filename="../src/duplicatpatterndlg.cpp" line="21"/>
+        <source>Cancel</source>
+        <translation>Abbrechen</translation>
+    </message>
+</context>
+<context>
+    <name>GenPatternDlg</name>
+    <message>
+        <location filename="../src/genpatterndlg.cpp" line="5"/>
+        <source>Generate Colour Pattern</source>
+        <translation>Generiere Farbmuster</translation>
+    </message>
+    <message>
+        <location filename="../src/genpatterndlg.cpp" line="6"/>
+        <source>Pattern</source>
+        <translation>Muster</translation>
+    </message>
+    <message>
+        <location filename="../src/genpatterndlg.cpp" line="15"/>
+        <source>Colour A</source>
+        <translation>Farbe A</translation>
+    </message>
+    <message>
+        <location filename="../src/genpatterndlg.cpp" line="16"/>
+        <source>Colour B</source>
+        <translation>Farbe B</translation>
+    </message>
+    <message>
+        <location filename="../src/genpatterndlg.cpp" line="17"/>
+        <source>Colour C</source>
+        <translation>Farbe C</translation>
+    </message>
+    <message>
+        <location filename="../src/genpatterndlg.cpp" line="18"/>
+        <source>Colour D</source>
+        <translation>Farbe D</translation>
+    </message>
+    <message>
+        <location filename="../src/genpatterndlg.cpp" line="47"/>
+        <source>Pattern shaft colours</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/genpatterndlg.cpp" line="48"/>
+        <source>Pattern line colours</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>MainWindow</name>
+    <message>
+        <location filename="../src/mainwindow.cpp" line="54"/>
+        <source>&amp;File</source>
+        <translation>&amp;Datei</translation>
+    </message>
+    <message>
+        <location filename="../src/mainwindow.cpp" line="55"/>
+        <source>&amp;Open</source>
+        <translation>&amp;Öffne</translation>
+    </message>
+    <message>
+        <location filename="../src/mainwindow.cpp" line="57"/>
+        <source>&amp;Save</source>
+        <translation>&amp;Speichern</translation>
+    </message>
+    <message>
+        <location filename="../src/mainwindow.cpp" line="58"/>
+        <source>Save &amp;as</source>
+        <translation>Speichern &amp;als</translation>
+    </message>
+    <message>
+        <location filename="../src/mainwindow.cpp" line="59"/>
+        <source>&amp;Print</source>
+        <translation>&amp;Drucke</translation>
+    </message>
+    <message>
+        <location filename="../src/mainwindow.cpp" line="60"/>
+        <source>&amp;Quit</source>
+        <translation>&amp;Beende</translation>
+    </message>
+    <message>
+        <location filename="../src/mainwindow.cpp" line="61"/>
+        <source>&amp;Edit</source>
+        <translation>&amp;Editieren</translation>
+    </message>
+    <message>
+        <location filename="../src/mainwindow.cpp" line="62"/>
+        <source>&amp;Undo</source>
+        <translation>&amp;Rückgängig</translation>
+    </message>
+    <message>
+        <location filename="../src/mainwindow.cpp" line="63"/>
+        <source>&amp;Redo</source>
+        <translation>&amp;Wiederherstellen</translation>
+    </message>
+    <message>
+        <location filename="../src/mainwindow.cpp" line="64"/>
+        <source>&amp;Move</source>
+        <translation>&amp;Verschiebe</translation>
+    </message>
+    <message>
+        <location filename="../src/mainwindow.cpp" line="65"/>
+        <source>&amp;Copy</source>
+        <translation>&amp;Kopieren</translation>
+    </message>
+    <message>
+        <location filename="../src/mainwindow.cpp" line="66"/>
+        <source>C&amp;lear</source>
+        <translation>&amp;Leeren</translation>
+    </message>
+    <message>
+        <location filename="../src/mainwindow.cpp" line="67"/>
+        <source>Mirror &amp;X</source>
+        <translation>Spiegel &amp;X</translation>
+    </message>
+    <message>
+        <location filename="../src/mainwindow.cpp" line="68"/>
+        <source>Mirror &amp;Y</source>
+        <translation>Spiegele &amp;Y</translation>
+    </message>
+    <message>
+        <location filename="../src/mainwindow.cpp" line="69"/>
+        <source>Generate colour &amp;pattern</source>
+        <translation>Generiere Farb&amp;muster</translation>
+    </message>
+    <message>
+        <location filename="../src/mainwindow.cpp" line="70"/>
+        <source>M&amp;odify pattern</source>
+        <translation>M&amp;odifiziere Muster</translation>
+    </message>
+    <message>
+        <location filename="../src/mainwindow.cpp" line="71"/>
+        <source>&amp;Duplicate pattern</source>
+        <translation>&amp;Dupliziere Muster</translation>
+    </message>
+    <message>
+        <location filename="../src/mainwindow.cpp" line="73"/>
+        <source>set cursor &amp;a</source>
+        <translation>setze Cursor &amp;a</translation>
+    </message>
+    <message>
+        <location filename="../src/mainwindow.cpp" line="75"/>
+        <source>set cursor &amp;b</source>
+        <translation>setze Cursor &amp;b</translation>
+    </message>
+    <message>
+        <location filename="../src/mainwindow.cpp" line="77"/>
+        <source>&amp;View</source>
+        <translation>&amp;Ansicht</translation>
+    </message>
+    <message>
+        <location filename="../src/mainwindow.cpp" line="78"/>
+        <source>View from &amp;bottom</source>
+        <translation>Ansicht von &amp;unten</translation>
+    </message>
+    <message>
+        <location filename="../src/mainwindow.cpp" line="80"/>
+        <source>&amp;Analyze pattern</source>
+        <translation>&amp;Analysiere Muster</translation>
+    </message>
+    <message>
+        <location filename="../src/mainwindow.cpp" line="81"/>
+        <source>&amp;Options</source>
+        <translation>&amp;Optionen</translation>
+    </message>
+    <message>
+        <location filename="../src/mainwindow.cpp" line="82"/>
+        <source>&amp;Config</source>
+        <translation>&amp;Einstellungen</translation>
+    </message>
+    <message>
+        <location filename="../src/mainwindow.cpp" line="83"/>
+        <source>&amp;Reset Colours</source>
+        <translation>&amp;Setze Farben zurück</translation>
+    </message>
+    <message>
+        <location filename="../src/mainwindow.cpp" line="84"/>
+        <source>&amp;About</source>
+        <translation>&amp;Über</translation>
+    </message>
+    <message>
+        <location filename="../src/mainwindow.cpp" line="166"/>
+        <location filename="../src/mainwindow.cpp" line="178"/>
+        <source>Open Weave</source>
+        <translation>Öffne Webmuster</translation>
+    </message>
+    <message>
+        <location filename="../src/mainwindow.cpp" line="166"/>
+        <location filename="../src/mainwindow.cpp" line="178"/>
+        <source>QWeave Files (*.weave);; WIF Files (*.wif)</source>
+        <translation>QWeave Dateien (*.weave);; WIF Dateien (*.wif)</translation>
+    </message>
+    <message>
+        <location filename="../src/mainwindow.cpp" line="208"/>
+        <source>About QWeave</source>
+        <translation>Über QWeave</translation>
+    </message>
+    <message>
+        <location filename="../src/mainwindow.cpp" line="209"/>
+        <source>Version %1
+Written by Jan Sundermeyer (C) 2017-2022
+Simple schematic/diagram entry editor.</source>
+        <translation>Version %1
+Geschrieben von Jan Sundermeyer (C) 2017-2022
+Webmuster Generator</translation>
+    </message>
+</context>
+<context>
+    <name>Modifydlg</name>
+    <message>
+        <location filename="../src/modifydlg.cpp" line="5"/>
+        <source>mirror horizontal</source>
+        <translation>spiegele horizontal</translation>
+    </message>
+    <message>
+        <location filename="../src/modifydlg.cpp" line="7"/>
+        <source>mirror vertical</source>
+        <translation>spiegele vertikal</translation>
+    </message>
+</context>
+<context>
+    <name>Weave</name>
+    <message>
+        <location filename="../src/weave.cpp" line="827"/>
+        <source>heddle %1 %2 
+</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/weave.cpp" line="830"/>
+        <source>analyze heddles</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>configDialog</name>
+    <message>
+        <location filename="../src/configdialog.cpp" line="24"/>
+        <source>Columns</source>
+        <translation>Spalten</translation>
+    </message>
+    <message>
+        <location filename="../src/configdialog.cpp" line="30"/>
+        <source>Lines</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/configdialog.cpp" line="36"/>
+        <source>Shafts</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/configdialog.cpp" line="42"/>
+        <source>Positions</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/configdialog.cpp" line="48"/>
+        <source>up</source>
+        <translation>hoch</translation>
+    </message>
+    <message>
+        <location filename="../src/configdialog.cpp" line="49"/>
+        <source>down</source>
+        <translation>runter</translation>
+    </message>
+    <message>
+        <location filename="../src/configdialog.cpp" line="56"/>
+        <source>exclusive shafts</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/configdialog.cpp" line="62"/>
+        <source>OK</source>
+        <translation>OK</translation>
+    </message>
+    <message>
+        <location filename="../src/configdialog.cpp" line="63"/>
+        <source>Cancel</source>
+        <translation>Abbrechen</translation>
+    </message>
+</context>
 </TS>
--- translation/qweave_en_US.ts
+++ translation/qweave_en_US.ts
@@ -1,4 +1,307 @@
 <?xml version="1.0" encoding="utf-8"?>
 <!DOCTYPE TS>
 <TS version="2.1" language="en_US">
+<context>
+    <name>DuplicatPatternDlg</name>
+    <message>
+        <location filename="../src/duplicatpatterndlg.cpp" line="5"/>
+        <source>Duplicate Pattern</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/duplicatpatterndlg.cpp" line="6"/>
+        <source>shift by:</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/duplicatpatterndlg.cpp" line="7"/>
+        <source>times:</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/duplicatpatterndlg.cpp" line="19"/>
+        <source>OK</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/duplicatpatterndlg.cpp" line="21"/>
+        <source>Cancel</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>GenPatternDlg</name>
+    <message>
+        <location filename="../src/genpatterndlg.cpp" line="5"/>
+        <source>Generate Colour Pattern</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/genpatterndlg.cpp" line="6"/>
+        <source>Pattern</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/genpatterndlg.cpp" line="15"/>
+        <source>Colour A</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/genpatterndlg.cpp" line="16"/>
+        <source>Colour B</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/genpatterndlg.cpp" line="17"/>
+        <source>Colour C</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/genpatterndlg.cpp" line="18"/>
+        <source>Colour D</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/genpatterndlg.cpp" line="47"/>
+        <source>Pattern shaft colours</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/genpatterndlg.cpp" line="48"/>
+        <source>Pattern line colours</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>MainWindow</name>
+    <message>
+        <location filename="../src/mainwindow.cpp" line="54"/>
+        <source>&amp;File</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/mainwindow.cpp" line="55"/>
+        <source>&amp;Open</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/mainwindow.cpp" line="57"/>
+        <source>&amp;Save</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/mainwindow.cpp" line="58"/>
+        <source>Save &amp;as</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/mainwindow.cpp" line="59"/>
+        <source>&amp;Print</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/mainwindow.cpp" line="60"/>
+        <source>&amp;Quit</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/mainwindow.cpp" line="61"/>
+        <source>&amp;Edit</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/mainwindow.cpp" line="62"/>
+        <source>&amp;Undo</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/mainwindow.cpp" line="63"/>
+        <source>&amp;Redo</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/mainwindow.cpp" line="64"/>
+        <source>&amp;Move</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/mainwindow.cpp" line="65"/>
+        <source>&amp;Copy</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/mainwindow.cpp" line="66"/>
+        <source>C&amp;lear</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/mainwindow.cpp" line="67"/>
+        <source>Mirror &amp;X</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/mainwindow.cpp" line="68"/>
+        <source>Mirror &amp;Y</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/mainwindow.cpp" line="69"/>
+        <source>Generate colour &amp;pattern</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/mainwindow.cpp" line="70"/>
+        <source>M&amp;odify pattern</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/mainwindow.cpp" line="71"/>
+        <source>&amp;Duplicate pattern</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/mainwindow.cpp" line="73"/>
+        <source>set cursor &amp;a</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/mainwindow.cpp" line="75"/>
+        <source>set cursor &amp;b</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/mainwindow.cpp" line="77"/>
+        <source>&amp;View</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/mainwindow.cpp" line="78"/>
+        <source>View from &amp;bottom</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/mainwindow.cpp" line="80"/>
+        <source>&amp;Analyze pattern</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/mainwindow.cpp" line="81"/>
+        <source>&amp;Options</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/mainwindow.cpp" line="82"/>
+        <source>&amp;Config</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/mainwindow.cpp" line="83"/>
+        <source>&amp;Reset Colours</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/mainwindow.cpp" line="84"/>
+        <source>&amp;About</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/mainwindow.cpp" line="166"/>
+        <location filename="../src/mainwindow.cpp" line="178"/>
+        <source>Open Weave</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/mainwindow.cpp" line="166"/>
+        <location filename="../src/mainwindow.cpp" line="178"/>
+        <source>QWeave Files (*.weave);; WIF Files (*.wif)</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/mainwindow.cpp" line="208"/>
+        <source>About QWeave</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/mainwindow.cpp" line="209"/>
+        <source>Version %1
+Written by Jan Sundermeyer (C) 2017-2022
+Simple schematic/diagram entry editor.</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>Modifydlg</name>
+    <message>
+        <location filename="../src/modifydlg.cpp" line="5"/>
+        <source>mirror horizontal</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/modifydlg.cpp" line="7"/>
+        <source>mirror vertical</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>Weave</name>
+    <message>
+        <location filename="../src/weave.cpp" line="827"/>
+        <source>heddle %1 %2 
+</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/weave.cpp" line="830"/>
+        <source>analyze heddles</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>configDialog</name>
+    <message>
+        <location filename="../src/configdialog.cpp" line="24"/>
+        <source>Columns</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/configdialog.cpp" line="30"/>
+        <source>Lines</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/configdialog.cpp" line="36"/>
+        <source>Shafts</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/configdialog.cpp" line="42"/>
+        <source>Positions</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/configdialog.cpp" line="48"/>
+        <source>up</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/configdialog.cpp" line="49"/>
+        <source>down</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/configdialog.cpp" line="56"/>
+        <source>exclusive shafts</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/configdialog.cpp" line="62"/>
+        <source>OK</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/configdialog.cpp" line="63"/>
+        <source>Cancel</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
 </TS>
